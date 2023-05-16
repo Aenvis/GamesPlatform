d88de3b3864048eb93014a06e6198ab6a2b73584
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-05-16 20:06:01 +0200

    User and Localization models

---
+++ GamesPlatform.Domain/Models/Localization.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesPlatform.Domain.Models
+{
+    public class Localization
+    {
+        public string Country { get; protected set; }
+    }
+}
--- GamesPlatform.Domain/Models/User.cs
+++ GamesPlatform.Domain/Models/User.cs
@@ -2,6 +2,8 @@ namespace GamesPlatform.Domain.Models
 {
     public class User
     {
-        
+        public Guid Id { get; protected set; }
+        public string Username { get; protected set; }
+        public string Fullname { get; protected set; }
     }
 }
