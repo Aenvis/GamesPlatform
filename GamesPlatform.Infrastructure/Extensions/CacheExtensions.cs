ba8648d3015485debdb332d8ca3b901eb93ba6fb
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-06-26 18:15:54 +0200

    Implement MemoryCache extensions for Jwt operations (Set, Get, GetKey)

---
+++ GamesPlatform.Infrastructure/Extensions/CacheExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GamesPlatform.Infrastructure.DTOs;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace GamesPlatform.Infrastructure.Extensions
+{
+    public static class CacheExtensions
+    {
+        public static void SetJwt(this IMemoryCache memoryCache, Guid tokenId, JwtDto jwt)
+        {
+            memoryCache.Set(GetJwtKey(tokenId), jwt, TimeSpan.FromSeconds(5));
+        }
+
+        public static JwtDto? GetJwt(this IMemoryCache memoryCache, Guid tokenId) => memoryCache.Get<JwtDto>(GetJwtKey(tokenId));
+
+        private static string GetJwtKey(Guid tokenId) => $"jwt-{tokenId}";
+    }
+}
