f8cdf9ba3ab958feb342c2afe4bff3e48c668551
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-06-27 19:10:15 +0200

    Add IGameService and its implementation

---
+++ GamesPlatform.Infrastructure/Services/GameService.cs
@@ -0,0 +1,33 @@
+﻿using GamesPlatform.Domain.Models;
+using GamesPlatform.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesPlatform.Infrastructure.Services
+{
+    public class GameService : IGameService
+    {
+        public GameService()
+        {
+            
+        }
+
+        public async Task AddNewGame(Guid id, string Title, string Author, string? Description = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<IEnumerable<GameDto>>> GetAllGamesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<GameDto>> GetGameAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GamesPlatform.Infrastructure/Services/IGameService.cs
+++ GamesPlatform.Infrastructure/Services/IGameService.cs
@@ -6,5 +6,7 @@ namespace GamesPlatform.Infrastructure.Services
     public interface IGameService : IService
     {
         Task<ServiceResponse<GameDto>> GetGameAsync(string title);
+        Task<ServiceResponse<IEnumerable<GameDto>>> GetAllGamesAsync();
+        Task AddNewGame(Guid id, string Title, string Author, string? Description = null);
     }
 }
