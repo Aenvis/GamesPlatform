a8a9cb2bee8cd9a0b9110a4210799cc666ca872f
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-07-10 23:37:56 +0200

    Implement UserGameNodeDto and Service interface

---
+++ GamesPlatform.Infrastructure/DTOs/UserGameNodeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GamesPlatform.Infrastructure.DTOs
+{
+    public class UserGameNodeDto
+    {
+        public Guid UserId { get; set; }
+        public Guid GameId { get; set; }
+    }
+}
---
+++ GamesPlatform.Infrastructure/Services/IUserLibraryService.cs
@@ -0,0 +1,12 @@
+﻿using GamesPlatform.Domain.Models;
+using GamesPlatform.Infrastructure.DTOs;
+
+namespace GamesPlatform.Infrastructure.Services
+{
+    public interface IUserLibraryService : IService
+    {
+        Task<ServiceResponse<IEnumerable<UserGameNodeDto>>> GetAllGamesOfOneUserAsync();
+        Task AddGameAsync(Guid userId, Guid gameId);
+        Task DeleteGameAsync(Guid userId, Guid gameId);
+    }
+}
